48ade1b718c213f6368517aea833a1fbc01c088f
AnthonyGuardado <mg25001@itcha.edu.sv>
2026-05-08 21:24:47 -0600

    Commit Inicial Tony al Ataque

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. Vino tony al ataque
      */
     public function up(): void
     {
