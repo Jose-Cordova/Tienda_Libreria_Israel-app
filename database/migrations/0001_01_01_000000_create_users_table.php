56e7f68cb68198b9526e8aa53f24c0ab4d777685
jamie maciel flores palacios <fp25001@itcha.edu.sv>
2026-05-12 20:00:33 -0600

    listo el jwt

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('estado', ['ACTIVO', 'INACTIVO'])->default('ACTIVO');
+            $table->foreignId('rol_id')->constrained('roles'); // FK hacia roles
             $table->rememberToken();
             $table->timestamps();
         });
